54f1c8fda116df4a9ab9296c01e59e660e4eea88
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-12 21:24:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,40 @@
 def create_an_empty_array
-  
+  []
 end
 
-def create_an_array
-  
+def create_an_array 
+ people_family = ["paula","sonia","bryanna","karina"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+people_family  <<["elliot"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ people_family.unshift("lissete")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  people_family.pop("tio")
 end
 
 def remove_element_from_start_of_array(array)
-  
+  people_family.shift("tati")
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+ people_family[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  people_family[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  people_family[-1]
 end
 
 def update_element_from_index(array, index_number, element)
+  people_family[3]= "Penny"
 
 end
